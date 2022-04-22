35567496cfde483704b09d57ff085573f2722bc6
Maxwell Nderitu <nderitumaxwell@gmail.com>
2022-04-23 02:16:39 +0300

    Initial commit for Nuxt 3

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Doughnut />
+</template>
+
+<script>
+import Doughnut from '../components/Doughnut'
+
+export default {
+  name: 'index',
+  components: { Doughnut }
+}
+</script>
+
+<style scoped>
+body, html {
+  width: 100%;
+  height: 100%;
+}
+</style>
